ab3ab6a8bc219434151b636584858a9aa6d4794f
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-04-17 12:27:22 -0300

    📦 Update Challenges

---
+++ fashionista/build/precache-manifest.197fd3db1f89eaaba688c780c352b599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3cc5388f64702bca6d",
+    "url": "/static/css/main.5d8209a6.chunk.css"
+  },
+  {
+    "revision": "8a3cc5388f64702bca6d",
+    "url": "/static/js/main.8a3cc538.chunk.js"
+  },
+  {
+    "revision": "7173e12873f0dd49431f",
+    "url": "/static/js/1.7173e128.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f9c960908b1da3e2136492c0eff438d",
+    "url": "/static/media/Brand.3f9c9609.svg"
+  },
+  {
+    "revision": "653c18df57dcf4aab9889ec43de460c0",
+    "url": "/index.html"
+  }
+];

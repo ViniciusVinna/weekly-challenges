016d23f20a56442a19a8d45b0568010de6f0c2d6
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-04-20 15:27:26 -0300

    🎉 Initial commit

--- react-contacts/src/modules/helpers.js
+++
@@ -1,7 +0,0 @@
-export const convertToLowercase = (value) => {
-  if (value.length) {
-    return value.toLowerCase();
-  }
-
-  return value;
-};
--- react-fashionista/src/components/Brand/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ReactComponent as LogoSVG } from './Brand.svg';
-
-const Brand = props => <LogoSVG {...props} />;
-
-export default Brand;
